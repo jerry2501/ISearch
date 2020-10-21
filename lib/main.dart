ac5464c95206658cb37aa1796e77315eb80aead3
jerry2501 <jigneshprajapati9924@gmail.com>
2020-10-21 10:40:16 +0530

    update here

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       ),
       
       home:SplashScreenView(
-        imageSrc: '',
+        imageSrc: 'assets/playstore.png',
         home: TfliteHome(),
         duration: 300,
         text: "ISearch",
